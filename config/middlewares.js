240468c4d982c760b1a43f7ba040d6d83361ecad
Michael Schmid <michael.schmid@capsloq.de>
2022-12-06 23:51:24 +0100

    cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,21 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: [
+        'http://localhost:1337',
+        'http://127.0.0.1:1337',
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
+        'https://squeamish-befitting-cushion.strapiapp.com'
+      ]
+    }
+  },
   'strapi::logger',
   'strapi::query',
   'strapi::body',
