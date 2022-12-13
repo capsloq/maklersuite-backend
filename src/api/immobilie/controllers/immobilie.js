b5a7789e0805ae2a550bea49ca7ecb894c156485
Michael Schmid <michael.schmid@capsloq.de>
2022-12-13 22:42:30 +0100

    anky stinkt und bildupload funktioniert bei create

--- src/api/immobilie/controllers/immobilie.js
+++ src/api/immobilie/controllers/immobilie.js
@@ -8,10 +8,13 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::immobilie.immobilie' , ({ strapi }) => ({
     // Method 3: Replacing a core action
-      async create(ctx) {
+      async create(ctx) {        
         // 1. Get user Id
+        // Wenn Controller files erwartet muss er überschrieben werden mit dem Feldnamen, dass die Bilder bekommen soll. Siehe unten im create
+        const files = ctx.request.files
+        console.log("🚀 ~ file: immobilie.js:15 ~ create ~ files", files)
         const { id: userId } = ctx.state.user;
-        console.log("🚀 ~ file: immobilie.js:14 ~ create ~ userId", userId)
+       
 
         // Get user field in Makler Table 
    
@@ -24,25 +27,50 @@ module.exports = createCoreController('api::immobilie.immobilie' , ({ strapi }) 
             return ctx.throw(404, 'Makler not found');
           }
 
+
+          if (!ctx.request.body) 
+          return ctx.throw(400, 'No body provided');
+
         
 
         // 2. Get body
-        const { data } = ctx.request.body;
+        const { data } = ctx.request.body
+        const parsedData = JSON.parse(data)
+        console.log("🚀 ~ file: immobilie.js:36 ~ create ~ data", data)
 
         // 3. Create entity       
         const mergeBodyWithMakler = {
-            ...data,
+            ...parsedData,
             makler: makler.id
         }
         console.log("🚀 ~ file: immobilie.js:21 ~ create ~ mergeBodyWithMakler", mergeBodyWithMakler)
         
 
-        const entity = await strapi.service('api::immobilie.immobilie').create({data: mergeBodyWithMakler});
+    const entity = await strapi.service('api::immobilie.immobilie').create(
+      {
+        data: mergeBodyWithMakler,        
+        files: {
+          file: files['files.bilder']
+        }
+      },
+
+
+
+    );
 
 
         // 4. Sanitize entity
         const sanitizedEntity = await this.sanitizeOutput(entity, ctx);    
         return this.transformResponse(sanitizedEntity);
       },
+
+      // async update(ctx) {
+      //   console.log("ctx.req", ctx.req.body, ctx.req.headers, ctx.req.fil)
+      //   // call normal behaviour via super
+      //   const { data, meta } = await super.find(ctx);
+      //   return { data, meta };
+
+
+      // }
     }));
     
